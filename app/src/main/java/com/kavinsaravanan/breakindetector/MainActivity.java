73ce150146e9cadbfcfc0a86f6526c161544a839
Kavin Saravanan <kavinsaravan@gmail.com>
2021-12-28 14:02:43 -0800

    Adding microphone permission

--- app/src/main/java/com/kavinsaravanan/breakindetector/MainActivity.java
+++ app/src/main/java/com/kavinsaravanan/breakindetector/MainActivity.java
@@ -1,14 +1,28 @@
 package com.kavinsaravanan.breakindetector;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.sensorapp.impactlib.PermissionActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends PermissionActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestMicrophonePermission();
+    }
+
+    @Override
+    public void onMicrophonePermissionGranted() {
+        super.onMicrophonePermissionGranted();
+        Toast.makeText(this, "Permission granted!!!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onMicrophonePermissionDenied() {
+        super.onMicrophonePermissionDenied();
+        Toast.makeText(this, "Sorry, permission denied!!!", Toast.LENGTH_LONG).show();
     }
 }
